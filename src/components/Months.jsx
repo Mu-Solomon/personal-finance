77bcc16dfce68f2e2a4e3bcaaede5512f2d86013
Mu-Solomon <musolomoni@gmail.com>
2024-01-10 23:38:54 +0300

    Removed first dummy data

--- src/components/Months.jsx
+++ src/components/Months.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 
 function Months(props) {
+  console.log(props.months);
   return (
     <div id="month-cards" className="grid grid-cols-6 gap-4 mt-5">
+      {props.months.map((month, key) => (
+        <div id="card" className="shadow-md px-4 py-2  rounded">
+          <span className="flex items-center justify-start gap-2">
+            <div className="relative">
+              <div className="w-4 h-4 bg-blue-500 rounded-full flex items-center justify-center">
+                <div className="w-2 h-2 bg-white rounded-full"></div>
+              </div>
+            </div>
+            <h2 className="font-bold">{month.name}</h2>
+          </span>
+
+          <p className="py-2 text-sm">
+            Income: Ugx{" "}
+            {month.days
+              .reduce((accumulator, previous) => {
+                return accumulator + previous.cashIn;
+              }, 0)
+              .toLocaleString("en-US")}
+          </p>
+          <p className="py-2 text-sm">
+            Savings: Ugx{" "}
+            {month.days
+              .reduce((accumulator, previous) => {
+                return accumulator + previous.cashSaved;
+              }, 0)
+              .toLocaleString("en-US")}
+          </p>
+        </div>
+      ))}
       <div id="card" className="shadow-md px-4 py-2  rounded">
         <span className="flex items-center justify-start gap-2">
           <div className="relative">
@@ -13,151 +43,8 @@ function Months(props) {
           <h2 className="font-bold">January</h2>
         </span>
 
-        <p className="py-2 text-sm">Income: Ugx 32,000,000</p>
-        <p className="py-2 text-sm">Savings: Ugx 4,000,000</p>
-      </div>
-      <div id="card" className="shadow-md px-4 py-2  rounded">
-        <span className="flex items-center justify-start gap-2">
-          <div className="relative">
-            <div className="w-4 h-4 bg-blue-500 rounded-full flex items-center justify-center">
-              <div className="w-2 h-2 bg-white rounded-full"></div>
-            </div>
-          </div>
-          <h2 className="font-bold">January</h2>
-        </span>
-
-        <p className="py-2 text-sm">Income: Ugx 32,000,000</p>
-        <p className="py-2 text-sm">Savings: Ugx 4,000,000</p>
-      </div>{" "}
-      <div id="card" className="shadow-md px-4 py-2  rounded">
-        <span className="flex items-center justify-start gap-2">
-          <div className="relative">
-            <div className="w-4 h-4 bg-blue-500 rounded-full flex items-center justify-center">
-              <div className="w-2 h-2 bg-white rounded-full"></div>
-            </div>
-          </div>
-          <h2 className="font-bold">January</h2>
-        </span>
-
-        <p className="py-2 text-sm">Income: Ugx 32,000,000</p>
-        <p className="py-2 text-sm">Savings: Ugx 4,000,000</p>
-      </div>{" "}
-      <div id="card" className="shadow-md px-4 py-2  rounded">
-        <span className="flex items-center justify-start gap-2">
-          <div className="relative">
-            <div className="w-4 h-4 bg-green-500 rounded-full flex items-center justify-center">
-              <div className="w-2 h-2 bg-white rounded-full"></div>
-            </div>
-          </div>
-          <h2 className="font-bold">January</h2>
-        </span>
-
-        <p className="py-2 text-sm">Income: Ugx 32,000,000</p>
-        <p className="py-2 text-sm">Savings: Ugx 4,000,000</p>
-      </div>{" "}
-      <div id="card" className="shadow-md px-4 py-2  rounded">
-        <span className="flex items-center justify-start gap-2">
-          <div className="relative">
-            <div className="w-4 h-4 bg-orange-500 rounded-full flex items-center justify-center">
-              <div className="w-2 h-2 bg-white rounded-full"></div>
-            </div>
-          </div>
-          <h2 className="font-bold">January</h2>
-        </span>
-
-        <p className="py-2 text-sm">Income: Ugx 32,000,000</p>
-        <p className="py-2 text-sm">Savings: Ugx 4,000,000</p>
-      </div>{" "}
-      <div id="card" className="shadow-md px-4 py-2  rounded">
-        <span className="flex items-center justify-start gap-2">
-          <div className="relative">
-            <div className="w-4 h-4 bg-blue-500 rounded-full flex items-center justify-center">
-              <div className="w-2 h-2 bg-white rounded-full"></div>
-            </div>
-          </div>
-          <h2 className="font-bold">January</h2>
-        </span>
-
-        <p className="py-2 text-sm">Income: Ugx 32,000,000</p>
-        <p className="py-2 text-sm">Savings: Ugx 4,000,000</p>
-      </div>{" "}
-      <div id="card" className="shadow-md px-4 py-2  rounded">
-        <span className="flex items-center justify-start gap-2">
-          <div className="relative">
-            <div className="w-4 h-4 bg-blue-500 rounded-full flex items-center justify-center">
-              <div className="w-2 h-2 bg-white rounded-full"></div>
-            </div>
-          </div>
-          <h2 className="font-bold">January</h2>
-        </span>
-
-        <p className="py-2 text-sm">Income: Ugx 32,000,000</p>
-        <p className="py-2 text-sm">Savings: Ugx 4,000,000</p>
-      </div>{" "}
-      <div id="card" className="shadow-md px-4 py-2  rounded">
-        <span className="flex items-center justify-start gap-2">
-          <div className="relative">
-            <div className="w-4 h-4 bg-blue-500 rounded-full flex items-center justify-center">
-              <div className="w-2 h-2 bg-white rounded-full"></div>
-            </div>
-          </div>
-          <h2 className="font-bold">January</h2>
-        </span>
-
-        <p className="py-2 text-sm">Income: Ugx 32,000,000</p>
-        <p className="py-2 text-sm">Savings: Ugx 4,000,000</p>
-      </div>{" "}
-      <div id="card" className="shadow-md px-4 py-2  rounded">
-        <span className="flex items-center justify-start gap-2">
-          <div className="relative">
-            <div className="w-4 h-4 bg-yellow-500 rounded-full flex items-center justify-center">
-              <div className="w-2 h-2 bg-white rounded-full"></div>
-            </div>
-          </div>
-          <h2 className="font-bold">January</h2>
-        </span>
-
-        <p className="py-2 text-sm">Income: Ugx 32,000,000</p>
-        <p className="py-2 text-sm">Savings: Ugx 4,000,000</p>
-      </div>{" "}
-      <div id="card" className="shadow-md px-4 py-2  rounded">
-        <span className="flex items-center justify-start gap-2">
-          <div className="relative">
-            <div className="w-4 h-4 bg-blue-500 rounded-full flex items-center justify-center">
-              <div className="w-2 h-2 bg-white rounded-full"></div>
-            </div>
-          </div>
-          <h2 className="font-bold">January</h2>
-        </span>
-
-        <p className="py-2 text-sm">Income: Ugx 32,000,000</p>
-        <p className="py-2 text-sm">Savings: Ugx 4,000,000</p>
-      </div>{" "}
-      <div id="card" className="shadow-md px-4 py-2  rounded">
-        <span className="flex items-center justify-start gap-2">
-          <div className="relative">
-            <div className="w-4 h-4 bg-green-500 rounded-full flex items-center justify-center">
-              <div className="w-2 h-2 bg-white rounded-full"></div>
-            </div>
-          </div>
-          <h2 className="font-bold">January</h2>
-        </span>
-
-        <p className="py-2 text-sm">Income: Ugx 32,000,000</p>
-        <p className="py-2 text-sm">Savings: Ugx 4,000,000</p>
-      </div>{" "}
-      <div id="card" className="shadow-md px-4 py-2  rounded">
-        <span className="flex items-center justify-start gap-2">
-          <div className="relative">
-            <div className="w-4 h-4 bg-red-500 rounded-full flex items-center justify-center">
-              <div className="w-2 h-2 bg-white rounded-full"></div>
-            </div>
-          </div>
-          <h2 className="font-bold">January</h2>
-        </span>
-
-        <p className="py-2 text-sm">Income: Ugx 32,000,000</p>
-        <p className="py-2 text-sm">Savings: Ugx 4,000,000</p>
+        <p className="py-2 text-sm">Income: Ugx {props.cashInSum}</p>
+        <p className="py-2 text-sm">Savings: Ugx {props.cashSavedSum}</p>
       </div>
     </div>
   );
